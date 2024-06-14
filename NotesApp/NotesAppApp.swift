1b4f1f42805bd4f06953963cc02d4bbcc07eb02c
jcwoodard85 <127172564+jcwoodard85@users.noreply.github.com>
2024-06-14 16:55:37 -0700

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by Justin Woodard on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
